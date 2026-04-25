c992c3a5488d0ad54e1bc198cb18ba0446658a70
Aviroop Mishra <learningwithavim@gmail.com>
2026-04-25 14:35:23 -0400

    WIP: my changes to main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("Region Page/region.html")
+
+
+@app.route("/landing")
+def profile():
+    return render_template("Landing Page/landingPage.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=False, port=5000)
