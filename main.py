b8b241fde6c67bf16e1a1a84e3ec96bdbdd0e204
Aviroop Mishra <learningwithavim@gmail.com>
2026-04-25 15:21:24 -0400

    initial commit

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ def home():
     return render_template("Region Page/region.html")
 
 
+@app.route("/webpage")
+def information():
+    return render_template("Information Page/webpage.html")
+
+
 @app.route("/landing")
 def profile():
     return render_template("Landing Page/landingPage.html")
+
+
+@app.route("/interactive")
+def interactive():
+    return render_template("Interactive Page/simulation.html")
 
 
 if __name__ == "__main__":
